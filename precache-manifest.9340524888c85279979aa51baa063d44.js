a30cafa0b7d84084898338bf13195fe46023c53d
Charles <ellerkaresz@gmail.com>
2018-10-23 13:34:37 +0200

    Updates

---
+++ precache-manifest.9340524888c85279979aa51baa063d44.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8000f64a7cc249e64e3a",
+    "url": "/static/js/main.8000f64a.chunk.js"
+  },
+  {
+    "revision": "4fd91c74e51506835aa9",
+    "url": "/static/js/1.4fd91c74.chunk.js"
+  },
+  {
+    "revision": "8000f64a7cc249e64e3a",
+    "url": "/static/css/main.605092ae.chunk.css"
+  },
+  {
+    "revision": "f2a25584785a5958316429b15bc0efcd",
+    "url": "/index.html"
+  }
+];
